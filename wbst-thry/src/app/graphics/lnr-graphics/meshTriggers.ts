8f5c174c12f61d27de1310244293506bf45c5997
Athreya 🚀 <athreya@umich.edu>
2020-07-24 00:58:29 -0700

    Planet and moon will highlight on hover and added textures for the moon (thanks NASA)
    Also reorganized the Playground a little bit

---
+++ wbst-thry/src/app/graphics/lnr-graphics/meshTriggers.ts
@@ -0,0 +1,37 @@
+import * as Babylon from 'babylonjs';
+
+// Makes use of actions https://doc.babylonjs.com/how_to/how_to_use_actions
+export default class MeshTriggers {
+    /**
+    * Registers actions to a given mesh to enable highlighting the object with a given color
+    * 
+    * @param mesh - Object to highlight on hover
+    * @param highlightLayer - The Babylon EffectLayer associated with the given scene of the mesh
+    * @param color - Babylon rgb color used for the highlight
+    * 
+    * @returns void
+    * 
+    */
+    public static highlightOnHover(mesh: Babylon.Mesh, highlightLayer: Babylon.HighlightLayer, 
+                                       color: Babylon.Color3): void {
+        // OnPointerOverTrigger use for hover over
+        mesh.actionManager.registerAction(
+            new Babylon.ExecuteCodeAction(
+                Babylon.ActionManager.OnPointerOverTrigger,
+                function() {
+                    highlightLayer.addMesh(mesh, color);
+                }
+            )
+        );
+
+        // OnPointerOutTrigger use for hover off
+        mesh.actionManager.registerAction(
+            new Babylon.ExecuteCodeAction(
+                Babylon.ActionManager.OnPointerOutTrigger,
+                function() {
+                    highlightLayer.removeMesh(mesh);
+                }
+            )
+        );
+    }
+}
